d8972dec9cdb90487465f2ebad701d5fc40ff720
Hengyu Shi <hengyu816@outlook.com>
2022-06-03 12:48:06 +1200

    Nav collapse menu, may overflow

--- frontend/src/components/Layouts/Navbar.jsx
+++ frontend/src/components/Layouts/Navbar.jsx
@@ -15,7 +15,31 @@ const Navbar = () => {
   const toggleDialog = () => setShowDialog(!showDialog)
 
   return (
-    <nav className="navbar navbar-expand navbar-light">
+    <nav className="navbar navbar-expand-md navbar-light">
+      <section id="collapse-wrap">
+        <label class="navbar-toggler" for="menu-toggler">
+          <span class="navbar-toggler-icon"></span>
+        </label>
+        <input type="checkbox" id="menu-toggler" class="d-none"></input>
+        
+        {/* Collapse Nav Menu for Mobile */}
+        <ul id="collapse-menu">
+          <li class="nav-item">
+            <a href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a href="/Shop/All">Shop</a>
+          </li>
+          <li className="nav-item">
+            <a href="/">Contact</a>
+          </li>
+          <li className="nav-item">
+            <input type="text" id="collapse-search" className="form-control" placeholder="Search here" />
+          </li>
+        </ul>
+      </section>
+
+      {/* Expanded Nav Menu */}
       <ul className="navbar-nav mb-2 mb-lg-0 d-none d-md-flex">
         <li className="nav-item pe-3">
           <a className="nav-link" aria-current="page" href="/">Home</a>
@@ -33,7 +57,9 @@ const Navbar = () => {
       </h1>
 
       <section id="icons">
-        <div id="search"><i className="bi bi-search"></i></div>
+        <div id="search" className="d-none d-md-flex">
+          <i className="bi bi-search"></i>
+        </div>
         <div id="user">
           { userToken ? 
             <span onClick={() => toggleDialog()}><i className="bi bi-person"></i></span>
